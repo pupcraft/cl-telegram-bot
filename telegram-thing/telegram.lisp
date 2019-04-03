88ae78302d45a6913d070fd8afa09ab6aae811d8
terminal625 <zpersoncool@gmail.com>
2019-04-02 23:59:48 -0400

    whitelists, update object, bookmarks,

--- telegram-thing/telegram.lisp
+++ telegram-thing/telegram.lisp
@@ -14,19 +14,25 @@
    most-positive-fixnum
    10))
 
+(defparameter *previous-time* 0)
+(defparameter *current-time* 0)
 (defun what-time (&optional (time (local-time:now)))
   (local-time:timestamp-to-unix time))
 
-(defparameter *stop* t)
+(defparameter  *stop* t)
 ;;(one-process-iteration bot)
-(defun start (token)
+(defun start (token &optional user)
+  (when user
+    (whitelist-user user))
   (setf *stop* nil)
   (let ((bot (make-gun-bot token)))
     (let ((submitted 0))
       ;;FIXME::this is here to empty lost time. Bug?
       (fps-independent-timestep::tick *ticker* ((what-time)))
       (loop
-	 (fps-independent-timestep::tick *ticker* ((what-time))
+	 (setf *previous-time* *current-time*)
+	 (setf *current-time* (what-time))
+	 (fps-independent-timestep::tick *ticker* (*current-time*)
 	   (one-process-iteration bot))
 	 (multiple-value-bind (value existsp)
 	     (lparallel:try-receive-result *channel*)
@@ -40,8 +46,9 @@
 	     (lparallel:submit-task
 	      *channel*
 	      (lambda ()
-		(cl-telegram-bot::get-updates bot
-					      :timeout 10)))))
+		(mapcar 'create-update
+			(cl-telegram-bot::get-updates bot
+						      :timeout 10))))))
 	 (when *stop* (return))))))
 (defun stop ()
   (setf *stop* t))
@@ -92,53 +99,142 @@
      (asdf:system-source-directory :cl-telegram-bot)))
    :as :jsown))
 
+(defparameter *user-id-whitelist* ())
+(progn
+  (defun whitelist-user (user)
+    (pushnew user *user-id-whitelist* :test 'eql))
+  (defun ban-user (user)
+    (setf *user-id-whitelist*
+	  (remove user *user-id-whitelist*)))
+  (defun user-whitelisted-p (user)
+    (member user *user-id-whitelist* :test 'eql )))
+(defun throw-out-bad-updates (updates)
+  (remove-if-not (lambda (update)
+		   (user-whitelisted-p (update-user update)))
+		   updates))
+
 (defparameter *output* *standard-output*)
 (defparameter *live-chats* nil)
 (defparameter *chat-id* nil)
 
-(defun handle-updates (bot updates)
-  (dolist (update updates)   
-    (multiple-value-bind (chatid existsp) (mehfs '("message" "chat")
-						 update)
-      (print update)
-      ;;chatid is the id of the chat the update is from
-      (when existsp
-	;;add it to the live chats
-	(pushnew chatid *live-chats* :test 'equalp)
-	(setf *chat-id*
-	      (mehfs '("message" "chat" "id")
-		     update))
-	(cl-telegram-bot/bindings::delete-message
-	 bot
-	 (mehfs '("message" "chat" "id")
-		update)
-	 (mehfs '("message" "message_id")
-		update)
-	 )))))
+(defun handle-updates (bot update-objs)
+  (setf update-objs (throw-out-bad-updates update-objs))
+  (dolist (update-obj update-objs)
+    (print update-obj)
+    (let ((update (update-raw-data update-obj)))
+      (multiple-value-bind (chatid existsp) (mehfs '("message" "chat")
+						   update)
+	(print update)
+	;;chatid is the id of the chat the update is from
+	(when existsp
+	  ;;add it to the live chats
+	  (pushnew chatid *live-chats* :test 'equalp)
+	  (setf *chat-id*
+		(mehfs '("message" "chat" "id")
+		       update))
+	  (print
+	   (cl-telegram-bot/bindings::delete-message
+	    bot
+	    (mehfs '("message" "chat" "id")
+		   update)
+	    (mehfs '("message" "message_id")
+		   update)
+	    )))))))
+(defmacro while ((clause) &body body)
+  `(do ()
+       ((not ,clause))
+     ,@body))
+(defun next-thing-to-do-time ()
+  (cdr (first *todo-timeline*)))
 (defun one-process-iteration (bot)
   (cl-telegram-bot::with-locked-bot (bot)
+    (while ((let ((thing (next-thing-to-do-time)))
+	      (and (not (eq thing :end))
+		   (>= *current-time* thing))))
+      (print (pop *todo-timeline*))
+      )
+    
     ;;(print "what" *output*)
     (when (< 1 (length *live-chats*))
       (error "what the hell? why are there more than one chat?"))
 
      ;;;;This part initiates chats
-    (dolist (chat *live-chats*)
-      (cl-telegram-bot/bindings::send-message
-       bot
-       (jsown:val chat "id")
-       "sdff"))
+    (when (zerop (random 100))
+      (multiple-value-bind (url image) (test::random-bookmark-?)
+	(dolist (chat *live-chats*)
+	  (cl-telegram-bot/bindings::send-message
+	   bot
+	   (jsown:val chat "id")
+	   (if (string= "" image)
+	       "https://gamepedia.cursecdn.com/minecraft_gamepedia/c/c8/Wolf.png"
+	       image))
+	  (cl-telegram-bot/bindings::send-message
+	   bot
+	   (jsown:val chat "id")
+	   url
+	   ))))
 
-    (when *chat-id*
-      (cl-telegram-bot/bindings::send-message
-       bot
-       *chat-id*
-       "what"
-       :reply-markup
-       *testcase*))
+    (when (zerop (random 10))
+      (when *chat-id*
+	(cl-telegram-bot/bindings::send-message
+	 bot
+	 *chat-id*
+	 "what"
+	 :reply-markup
+	 *testcase*)))
+
+    (add-task "google.com" (what-time
+		   (local-time:timestamp+ (local-time:now)
+					  (random 100) :sec)))
     
      ;;;;This part responds to updates
     (print 3434)))
 
+(struct-to-clos:struct->class
+ (defstruct update
+   update_id
+   type
+   thing
+   raw-data
+   user))
+(defmethod print-object ((update update) stream)
+  (write
+   (list
+    (update-user update)
+    (update-update_id update)
+    (update-type update)
+    (update-thing update))
+   :stream stream))
+
+(defun create-update (update-data)
+  (let ((type nil)
+	(thing nil))
+    (block out
+      (dolist (item '("message"
+		      "edited_message"
+		      "channel_post"
+		      "edited_channel_post"
+		      "inline_query"
+		      "chosen_inline_result"
+		      "callback_query"
+		      "shipping_query"
+		      "pre_checkout_query"))
+	(multiple-value-bind (value existsp)
+	    (mehf
+	     item
+	     update-data)
+	  (when existsp
+	    (setf type item
+		  thing value)
+	    (return-from out)))))
+    (let ((user-id (mehf "from" thing)))
+      (make-update :update_id (mehf "update_id" update-data)
+		   :type type
+		   :thing thing
+		   :raw-data update-data
+		   :user (if user-id ;;FIXME::not pretty, see user-id above
+			     (mehf "id" user-id)
+			     nil)))))
 ;;;whitelisted users
 ;;;- input whitelist and bot key
 ;;;throw out updates not on the whitelist
@@ -147,3 +243,27 @@
 ;;;remember chat history, also sort by time
 ;;;persistent? 
 ;;;separation of polling and sending
+
+;;FIXME::O(N) because it uses a list, gets really slow, quadratically?
+(progn
+  (defparameter *todo-timeline* (list
+				 (cons "end" :end)))
+  (defun add-task (task time)
+    (let ((tail
+	   (member-if (lambda (a-time)
+			(if (eq a-time :end)
+			    t
+			    (> a-time time)))
+		      *todo-timeline* :key 'cdr)))
+      (destructive-insert-first-in-list
+       (cons task time)
+       tail))
+    *todo-timeline*)
+
+  (defun destructive-insert-first-in-list (item list)
+    (if (consp list)
+	(let ((old-car (car list)))
+	  (setf (car list) item
+		(cdr list) (cons old-car (cdr list)))
+	  list)
+	(error "not a list ~a" list))))
